8a7e9fe4f2331ad0006a5cd35dd34c834a9636aa
saisaran007 <sais26042@gmail.com>
2023-09-14 13:01:21 +0530

    conditional operators

---
+++ conditional.js
@@ -0,0 +1,27 @@
+let num = 15;
+if(num % 3 == 0)
+{
+console.log("multiple of three")
+}
+else{
+  console.log("not a multiple of three")
+}
+ let age = 25;
+ if(age >= 18)
+ {
+   console.log("Apply for license")
+ }
+else{
+  console.log("NA")
+}
+let a = 20;
+let b = 40;
+if(a > b){
+  console.log("A is Greater");
+}
+else if(b > a){
+  console.log("B is Greater");
+}
+else{
+  console.log("Both are Equal")
+}
